93fa242dc7c59b164e5de1e4ef154a6288a77d96
Dawson Rubino <155998903+drubino8@users.noreply.github.com>
2024-12-11 06:00:10 +0000

    Added analog clock

--- public/time.js
+++ public/time.js
@@ -65,3 +65,32 @@ const theClockApp = () => {
     // Re-run `theClockApp` every 1 second (1000 ms)
     setInterval(theClockApp, 1000);
     
+
+    // Analog Clock
+const secondHand = document.querySelector('.second-hand');
+const minsHand = document.querySelector('.min-hand');
+const hourHand = document.querySelector('.hour-hand');
+function setDate() {
+    const now = new Date();
+    const seconds = now.getSeconds();
+    const secondsDegrees = ((seconds / 60) * 360) + 90;
+    secondHand.style.transform = `rotate(${secondsDegrees}deg)`;
+    const mins = now.getMinutes();
+    const minsDegrees = ((mins / 60) * 360) + ((seconds / 60) * 6) + 90;
+    minsHand.style.transform = `rotate(${minsDegrees}deg)`;
+    const hour = now.getHours();
+    const hourDegrees = ((hour / 12) * 360) + ((mins / 60) * 30) + 90;
+    hourHand.style.transform = `rotate(${hourDegrees}deg)`;
+}
+
+setInterval(setDate, 1000);
+setDate();
+
+// GET Stuff
+const switcher = document.querySelector("#switcher");
+const body = document.querySelector("body");
+
+// SET Stuff
+switcher.addEventListener( "click" , () => {
+  body.classList.toggle("dark");
+} );
